e892e9898bf081da00b0ca06c8eb73d605596fed
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-01-17 13:43:00 +0100

    Added Point A click to spawn and teleport cube

---
+++ Assets/Scripts/PointA.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointA : MonoBehaviour
+{
+    [SerializeField]List<Transform> _waypoints;
+    [SerializeField] GameObject _cube;
+    GameObject spawnedCube;
+    int count;
+    
+    private void OnMouseDown() {
+        count = 0;
+        StartCoroutine("TeleportCube");
+    }
+    
+    private IEnumerator TeleportCube()
+    {
+        if(count == 0)
+        {
+        spawnedCube = Instantiate(_cube, _waypoints[count].transform.position,Quaternion.identity);
+        yield return new WaitForSeconds(2f);
+        count ++;
+        //Debug.Log("first coroutine " +count);
+        StartCoroutine("TeleportCube");
+        } else if (count < _waypoints.Count)
+        {
+            spawnedCube.transform.position = _waypoints[count].position;
+            count++;
+            //Debug.Log("subsequent coroutines" + count);
+            yield return new WaitForSeconds(2f);
+            StartCoroutine("TeleportCube");
+        }
+        else{
+            spawnedCube.SetActive(false);
+            StopCoroutine("TeleportCube");
+        }
+
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
